24e6cdb19fe3290c52831b37e8ff40294b8c87ad
Chido100 <chido_dozie@hotmail.com>
2024-10-22 18:08:22 +0100

    Created a connection manager

--- app/main.py
+++ app/main.py
@@ -13,4 +13,26 @@ class ConnectionManager:
         await websocket.accept()
         self.active_connections.append(websocket)
     
-    def disconnect(self, websocket)
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/w/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connet(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_message(f"Client {client_id}: {data}", websocket)
+            await manager.broadcast(f"Client {client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client {client_id} disconnected")
